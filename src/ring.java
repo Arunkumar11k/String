beaa70c836b23fffcb19467d0034622eeb2a49f3
Arunkumar11k <k.arunkumar11595@gmail.com>
2022-01-02 13:54:58 +0530

    ar

---
+++ src/ring.java
@@ -0,0 +1,16 @@
+
+public class ring {
+	
+ private void add() {
+
+	 System.out.println("Arun");
+	 System.out.println("Arun is smart boy");
+	 System.out.println("cibi is bad boy");
+	 System.out.println("yuvi very worst boy");
+}
+ public static void main(String[] args) {
+	ring s = new ring();
+	s.add();
+	
+}
+}
