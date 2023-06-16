37caf410836756b63cd40c5e569faa3a0f30a659
Arunkumar11k <96730109+Arunkumar11k@users.noreply.github.com>
2023-06-16 17:46:56 +0530

    Update ring.java

--- src/ring.java
+++ src/ring.java
@@ -2,7 +2,9 @@
 public class ring {
 	
  private void add() {
-
+	 
+     String s="Arunkumar";
+	 String rev="";
 	 System.out.println("Arun");
 	 System.out.println("Arun is smart boy");
 	 System.out.println("cibi is bad boy");
@@ -11,6 +13,9 @@ public class ring {
  public static void main(String[] args) {
 	ring s = new ring();
 	s.add();
+	 for(int i=s.length()-1;i>=0;i--){
+		rev=rev+s.charAt(i);
+	 }
 	
 }
 }
